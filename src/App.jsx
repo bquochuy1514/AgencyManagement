161af118bc37b780ced752b2ff5b753a7b3044ca
bquochuy1514 <23520594@gm.uit.edu.vn>
2025-06-04 21:14:00 +0700

    add folder template

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<h1 className="text-red-500 text-2xl font-bold"> Hello World! </h1>
+		</>
+	);
 }
 
 export default App;
