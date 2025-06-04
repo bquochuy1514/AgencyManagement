861f261cc3bfc10a1097cad5868ffc3ba0a1fa7a
bquochuy1514 <23520594@gm.uit.edu.vn>
2025-06-05 00:39:01 +0700

    add necessary routes:

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -15,33 +15,63 @@ const MainLayout = () => {
 			<div className="flex h-screen text-white">
 				<div className="fixed w-64 h-full bg-gray-800 p-4">
 					<div className="text-xl font-bold mb-6 flex items-center">
-						<span className="text-purple-500">Desk</span>
-						<span className="text-red-500">App</span>
+						<span className="text-purple-500">Agent</span>
+						<span className="text-red-500">Management</span>
 					</div>
 					<ul className="space-y-2 cursor-pointer">
-						<li className="flex items-center p-2 hover:bg-gray-700 rounded">
-							<FaPlusCircle className="w-5 h-5 mr-2" />
-							<Link to="/">Đại lý</Link>
+						<li>
+							<Link
+								to="/"
+								className="flex items-center p-2 hover:bg-gray-700 rounded"
+							>
+								<FaPlusCircle className="w-5 h-5 mr-2" />
+								Đại lý
+							</Link>
 						</li>
-						<li className="flex items-center p-2 hover:bg-gray-700 rounded">
-							<FaFileExport className="w-5 h-5 mr-2" />
-							Xuất hàng
+						<li>
+							<Link
+								to="/deliveries"
+								className="flex items-center p-2 hover:bg-gray-700 rounded"
+							>
+								<FaFileExport className="w-5 h-5 mr-2" />
+								Xuất hàng
+							</Link>
 						</li>
-						<li className="flex items-center p-2 hover:bg-gray-700 rounded">
-							<FaMoneyBillWave className="w-5 h-5 mr-2" />
-							Thu tiền
+						<li>
+							<Link
+								to="/payments"
+								className="flex items-center p-2 hover:bg-gray-700 rounded"
+							>
+								<FaMoneyBillWave className="w-5 h-5 mr-2" />
+								Thu tiền
+							</Link>
 						</li>
-						<li className="flex items-center p-2 hover:bg-gray-700 rounded">
-							<FaChartBar className="w-5 h-5 mr-2" />
-							Báo cáo
+						<li>
+							<Link
+								to="/reports"
+								className="flex items-center p-2 hover:bg-gray-700 rounded"
+							>
+								<FaChartBar className="w-5 h-5 mr-2" />
+								Báo cáo
+							</Link>
 						</li>
-						<li className="flex items-center p-2 hover:bg-gray-700 rounded">
-							<FaUserShield className="w-5 h-5 mr-2" />
-							Quy Định
+						<li>
+							<Link
+								to="/regulations"
+								className="flex items-center p-2 hover:bg-gray-700 rounded"
+							>
+								<FaUserShield className="w-5 h-5 mr-2" />
+								Quy Định
+							</Link>
 						</li>
-						<li className="flex items-center p-2 hover:bg-gray-700 rounded">
-							<FaBoxOpen className="w-5 h-5 mr-2" />
-							<Link to="/products">Hàng hoá</Link>
+						<li>
+							<Link
+								to="/products"
+								className="flex items-center p-2 hover:bg-gray-700 rounded"
+							>
+								<FaBoxOpen className="w-5 h-5 mr-2" />
+								Hàng hoá
+							</Link>
 						</li>
 					</ul>
 				</div>
