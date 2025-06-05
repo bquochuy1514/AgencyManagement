389d92fed5539d31045a57d25ddfb89eab8ecf97
bquochuy1514 <23520594@gm.uit.edu.vn>
2025-06-05 23:54:58 +0700

    push code moi

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,10 +4,6 @@ const AgentManagement = () => {
 	return (
 		<>
 			<h1 className="text-red-500">Trang chủ</h1>
-			<h1 className="text-3xl text-pink-600 font-bold mb-4 mt-8">
-				{' '}
-				Trang Lì như trâu như bò!!!
-			</h1>
 		</>
 	);
 };
